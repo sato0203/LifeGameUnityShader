89b5c1b300a89ec36f73eafdb1cdd5762ddc23af
AkiSato <sato.geijo.13@gmail.com>
2016-10-29 12:16:16 +0900

    init

---
+++ Assets/Script/LifeGameShaderManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeGameShaderManager : MonoBehaviour {
+
+	[SerializeField]
+	private Shader lifeGameShader;
+	[SerializeField]
+	private Shader unlitTextureShader;
+	[SerializeField]
+	private RenderTexture curRenderTexture;
+	[SerializeField]
+	private RenderTexture prevRenderTexture;
+	private Texture startTexture;
+	private Material copyMaterial;
+	[SerializeField]
+	private Material initMaterial;
+	private Material lifeGameMaterial;
+
+	bool flag = false;
+
+	private float elapsedSeconds;
+
+	// Use this for initialization
+	void Awake () {
+		copyMaterial = new Material(unlitTextureShader);
+		lifeGameMaterial = new Material(lifeGameShader);
+		Graphics.Blit(null, curRenderTexture, initMaterial);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsedSeconds += Time.deltaTime;
+		if (elapsedSeconds > 1f)
+		{
+			Graphics.Blit(curRenderTexture, prevRenderTexture, copyMaterial);
+			Graphics.Blit(prevRenderTexture, curRenderTexture, lifeGameMaterial);
+			elapsedSeconds = 0;
+			Debug.Log("aa");
+		}
+	}
+}
---
+++ Assets/Script/MeshInformationReader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshInformationReader : MonoBehaviour {
+
+	private MeshFilter meshFilter;
+
+	// Use this for initialization
+	void Start () {
+		meshFilter = GetComponent<MeshFilter>();
+		Debug.Log("vertices");
+		foreach (var obj in meshFilter.mesh.vertices)
+		{
+			Debug.Log(obj);
+		}
+		Debug.Log("triangles");
+		foreach (var obj in meshFilter.mesh.triangles)
+		{
+			Debug.Log(obj);
+		}
+		Debug.Log("uvs");
+		foreach (var obj in meshFilter.mesh.uv)
+		{
+			Debug.Log(obj);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
